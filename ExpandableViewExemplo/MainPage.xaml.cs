5093e4045c1effb176e70bcee5e00c6855d6bb0e
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2019-06-11 10:11:00 -0300

    :tada:

---
+++ ExpandableViewExemplo/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Expandable;
+
+namespace ExpandableViewExemplo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+            lblStatus.Text = "Fechado";
+        }
+
+        public void Handle_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case ExpandStatus.Collapsing:
+                    lblStatus.Text = "Fechado";
+                    break;
+                case ExpandStatus.Expanding:
+                    lblStatus.Text = "Aberto";
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+}
